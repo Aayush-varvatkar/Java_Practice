80303c84139c58bba1c2e18dfeff431fbc54f782
Aayush-varvatkar <aayushvarvatkar@gmail.com>
2024-12-09 02:57:36 +0530

    Inheritance and acess specifiers compect

---
+++ Animal.java
@@ -0,0 +1,12 @@
+package Java_Practice;
+
+public class Animal {
+    void eat(){
+        System.out.println("Animals eat food");
+    }
+}
+class Dog extends Animal{
+    void Sound(){
+        System.out.println("Dogs can bark");
+    }
+}
---
+++ InheritanceDemo.java
@@ -0,0 +1,12 @@
+package Java_Practice;
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Animal A1 = new Animal();
+        A1.eat();
+
+        Dog D1 = new Dog();
+        D1.eat();
+        D1.Sound();
+    }
+}
