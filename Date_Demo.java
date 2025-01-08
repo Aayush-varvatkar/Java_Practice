8cba2e618585084a2fa7bda576d8055406fcfea6
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-08 16:24:23 +0530

    date implementation concept

---
+++ Date_Demo.java
@@ -0,0 +1,14 @@
+package Java_Practice;
+import java.util.Date;
+
+public class Date_Demo {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+
+        System.out.println(d.getTime());  
+        // System.out.println(d.getDate());
+        // System.out.println(d.getSeconds());
+        // System.out.println(d.getYear());
+    }   
+}
