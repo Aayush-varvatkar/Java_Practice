31115ba8153a20d7d1cd8f91553deda45681f9e1
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-08 22:20:52 +0530

    Time Implementation and concept

--- Time.java
+++ Time.java
@@ -1,5 +1,9 @@
 package Java_Practice;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Time {
     public static void main(String[] args) {
         
@@ -7,7 +11,19 @@ public class Time {
         System.out.println(System.currentTimeMillis() / 1000);  // returns the current time in seconds   - 
         System.out.println(System.currentTimeMillis() / 1000 / 3600);  // returns the current time in hours   - 
         System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24);  // returns the current time in days   - 
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365);  // returns the current time in years   -from 1 jan 1970
+        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365);  // returns the current time in years   -from 1 jan 1970 
+
+
+
+
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
 
     }
 }
