f64637bc93e879a47c288d7c8ec2ae6e3bfce0bd
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-12 23:20:31 +0530

    Generic implementation and concept

--- Generics_Demo.java
+++ Generics_Demo.java
@@ -1,5 +1,8 @@
 package Java_Practice;
 
+// feature that allows you to write code that can handle any type of object in a type-safe manner.
+// Generics_Demo and Generic_Print are connected files.
+
 public class Generics_Demo <T>{
     T thingToPrint;
 
